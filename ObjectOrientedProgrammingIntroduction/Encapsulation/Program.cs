2c225e79c6cabd5e7794df33571433be5cd1b58b
vyagi <marcin.jagiela@hotmail.com>
2023-12-05 16:06:31 +0100

    Constructor added

--- ObjectOrientedProgrammingIntroduction/Encapsulation/Program.cs
+++ ObjectOrientedProgrammingIntroduction/Encapsulation/Program.cs
@@ -2,11 +2,40 @@
 
 namespace Encapsulation
 {
+    class Student
+    {
+        private string _name;
+        private string _lastName;
+        private int _age;
+
+        public Student(string name, string lastName, int age)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException("Name is required");
+
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentNullException("Last name is required");
+
+            if (age < 16)
+                throw new ArgumentException("Age must be minimum 16");
+
+            _name = name;
+            _lastName = lastName;
+            _age = age;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var s = new Student("James", "Bond", 40);
+            Console.WriteLine($"{s.name} {s.lastName} is {s.age}.");
+
+            s.age = -10;
+            s.name = null;
+            s.lastName = "   ";
+            Console.WriteLine($"{s.name} {s.lastName} is {s.age}.");
         }
     }
 }
