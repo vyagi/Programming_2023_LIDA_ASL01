6f26ac737c960fedf79c0011d57eb7f05aa8fbec
vyagi <marcin.jagiela@hotmail.com>
2023-12-19 15:53:39 +0100

    Partially done

---
+++ ObjectOrientedProgrammingIntroduction/OOPRemainingConcepts/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace OOPRemainingConcepts
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Unspecified sound....");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Hau hau");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Miau miau");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new Dog();
+            d.MakeSound();
+
+            var c = new Cat();
+            c.MakeSound();
+
+            ProduceSounds(new Animal[] { d, c, new Dog(), new Cat() });
+        }
+
+        static void ProduceSounds(Animal[] animals)
+        {
+            foreach (var animal in animals)
+            {
+                animal.MakeSound();
+            }
+        }
+    }
+}
