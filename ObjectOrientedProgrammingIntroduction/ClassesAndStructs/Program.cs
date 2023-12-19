1d24d260b9c98f6547562a57a1884e92b98946ab
vyagi <marcin.jagiela@hotmail.com>
2023-12-19 16:30:59 +0100

    Struct finished

---
+++ ObjectOrientedProgrammingIntroduction/ClassesAndStructs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassesAndStructs
+{
+    public struct Student 
+    {
+        public string Name;
+        public int Age;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.Name = "James Bond";
+            s.Age = 40;
+            Console.WriteLine($"{s.Name} is {s.Age} years old");
+
+            Update(s);
+            Console.WriteLine($"{s.Name} is {s.Age} years old");
+
+        }
+
+        public static void Update(Student student)
+        {
+            student.Name = "Mr. Bean";
+            student.Age = 50;
+        }
+    }
+}
