85401fd520d95e632c3fc427ef7c990b94b1790d
vyagi <marcin.jagiela@hotmail.com>
2024-01-09 16:50:47 +0100

    Invoice App done

---
+++ ObjectOrientedProgrammingIntroduction/WinFormsIntro/InvoiceAppForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace WinFormsIntro
+{
+    partial class InvoiceAppForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.categoryButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(12, 49);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(13, 18);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(31, 15);
+            this.pathLabel.TabIndex = 1;
+            this.pathLabel.Text = "Path";
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(60, 10);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(728, 23);
+            this.pathTextBox.TabIndex = 2;
+            this.pathTextBox.Text = "C:\\Users\\mjagiela\\Desktop\\Invoices.txt";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(13, 79);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(775, 359);
+            this.resultTextBox.TabIndex = 3;
+            // 
+            // categoryButton
+            // 
+            this.categoryButton.Location = new System.Drawing.Point(103, 49);
+            this.categoryButton.Name = "categoryButton";
+            this.categoryButton.Size = new System.Drawing.Size(75, 23);
+            this.categoryButton.TabIndex = 4;
+            this.categoryButton.Text = "Categories";
+            this.categoryButton.UseVisualStyleBackColor = true;
+            this.categoryButton.Click += new System.EventHandler(this.categoryButton_Click);
+            // 
+            // InvoiceAppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.categoryButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.openButton);
+            this.Name = "InvoiceAppForm";
+            this.Text = "Invoice Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button categoryButton;
+    }
+}
+
---
+++ ObjectOrientedProgrammingIntroduction/WinFormsIntro/InvoiceAppForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WinFormsIntro
+{
+    public partial class InvoiceAppForm : Form
+    {
+        public InvoiceAppForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            var path = pathTextBox.Text;
+
+            var content = File.ReadAllText(path);
+
+            content = content.Replace(";", "\t");
+
+            resultTextBox.Text = content;
+        }
+
+        private void categoryButton_Click(object sender, EventArgs e)
+        {
+            var path = pathTextBox.Text;
+
+            var lines = File.ReadAllLines(path);
+
+            var result = new Dictionary<string, decimal>();
+
+            for (var i = 1; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                var split = line.Split(";");
+
+                var price = decimal.Parse(split[1]);
+                var category = split[2];
+
+                if (result.ContainsKey(category))
+                {
+                    result[category] += price;
+                }
+                else
+                {
+                    result[category] = price;
+                }
+            }
+
+            resultTextBox.Text = $"Category\tAmount{Environment.NewLine}";
+
+            foreach (var item in result)
+            {
+                resultTextBox.Text += $"{item.Key}\t{item.Value}{Environment.NewLine}";
+            }
+        }
+    }
+}
